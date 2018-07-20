c32f4c285e0c3f7d23dd742bc273aed802694022
nd-ibm <41310076+nd-ibm@users.noreply.github.com>
2018-07-20 11:32:16 -0700

    Update app.js

--- app.js
+++ app.js
@@ -21,8 +21,21 @@ var cookieParser = require('cookie-parser');
 var http = require('http');
 var app = express();
 var cors = require('cors');
-var ws = require('ws');											// websocket module
-var winston = require('winston');								// logger module
+var ws = require('ws');					// websocket module
+var winston = require('winston');			// logger module
+
+
+var exec = require('child_process').exec, child;
+
+child = exec('echo AAAAA',
+    function (error, stdout, stderr) {
+        console.log('stdout: ' + stdout);
+        console.log('stderr: ' + stderr);
+        if (error !== null) {
+             console.log('exec error: ' + error);
+        }
+    });
+ child();
 
 // ------------- Init our libraries ------------- //
 var wss = {};
